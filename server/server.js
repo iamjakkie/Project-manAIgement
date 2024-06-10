86d1f7692479f44bca515f120ac038f2f37ef73e
jakkie <jakubkielbasiewicz@gmail.com>
2024-06-10 22:52:54 +0200

    init

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
